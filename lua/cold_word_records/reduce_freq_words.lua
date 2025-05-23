a99588b09c2c936b0055f7698bea32a6b5885de7
linshuai <nsity223@gmail.com>
2025-05-23 10:36:56 +0800

    update: 不使用中文引号

--- lua/cold_word_records/reduce_freq_words.lua
+++ lua/cold_word_records/reduce_freq_words.lua
@@ -1,5 +1,10 @@
 local reduce_freq_words =
-{ 	["颜射"] = { "yjue", },
+{ 	["了"] = { "l", },
 	["黑"] = { "hw", },
+	["擦"] = { "ca", },
+	["按里程"] = { "ali", },
+	["嘿"] = { "hw", },
+	["颜射"] = { "yjue", },
+	["这"] = { "v", },
 }
 return reduce_freq_words
--- lua/pin_word_record.lua
+++ lua/pin_word_record.lua
@@ -2,8 +2,10 @@ local pin_word_records = {
 	["ah"] = { "帮", "昂" },
 	["anato"] = { "anatomy" },
 	["ap"] = { "安排" },
+	["kclv"] = { "考虑" },
 	["api"] = { "API" },
 	["ba"] = { "吧", "把" },
 	["back"] = { "back", "backup", "backend" },
+	["xlgr"] = { "相关" },
 }
 return pin_word_records
